8d0b9b3a4c6bd0b179b9bba38cdfd28581a4440c
손지연 <dolpini0426@gmail.com>
2023-11-03 17:47:16 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14024 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter br = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        br.write("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+        br.close();
+    }
+}
