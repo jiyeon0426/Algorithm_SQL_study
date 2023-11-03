2c81365d523a2e9d2935cac125751bf257340538
손지연 <dolpini0426@gmail.com>
2023-11-03 17:43:53 +0900

    [Bronze V] Title: We love kriii, Time: 200 ms, Memory: 17420 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+
+}
